22bce9906d3decd0b3d1ec908698e3151eb871cf
alexisrenteria02 <a220203871@unison.mx>
2022-01-27 11:00:31 -0700

    Commit

---
+++ ContadorTexto-master/src/main/java/WordCounterSplit.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WordCounterSplit {
+    public static void main(String[] args) {
+        if(args.length == 0){
+            System.out.println("falta el nombre del archivo.");
+            System.exit(1);
+        }
+        String fileName = args[0];
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(fileName);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encuentra el archivo");
+            System.exit(2);
+        }
+
+        BufferedReader in = new BufferedReader(fileReader);
+
+        String textLine = null;
+        int contadorPalabras = 0;
+        int contadorLineas = 0;
+        String linea;
+
+        long start = System.currentTimeMillis();
+        try {
+            while ((linea = in.readLine()) !=null){
+                String[] palabras = linea.split("\\s+");
+                contadorPalabras = contadorPalabras + palabras.length;
+                contadorLineas++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo");
+            System.exit(3);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        long time = System.currentTimeMillis() - start;
+
+        System.out.printf("El archivo %s tiene %,7d palabras. " , fileName, contadorPalabras);
+        System.out.printf(" Numero de lineas: %,6d%n", contadorLineas);
+        System.out.printf("Tiempo procesamiento (milisegundos): %d %n" , time);
+
+    }
+}
---
+++ ContadorTexto-master/src/main/java/WordCounterStringT.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCounterStringT {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Falta el nombre del archivo");
+            System.exit(1);
+        }
+
+        String fileName = args[0];
+
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("El nombre del archivo no se encuentra");
+            System.exit(2);
+        }
+
+        BufferedReader in = new BufferedReader(fileReader);
+
+
+        String textLine = null;
+        int contadorPalabras = 0;
+        int contadorLineas = 0;
+        String linea;
+
+        long start = System.currentTimeMillis();
+        try {
+            while ((linea = in.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(linea);
+                contadorPalabras = contadorPalabras + st.countTokens();
+                contadorLineas++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo");
+            System.exit(3);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        long time = System.currentTimeMillis() - start;
+
+        System.out.printf("El archivo %s tiene %,7d palabras. ", fileName, contadorPalabras);
+        System.out.printf(" Numero de lineas: %,6d%n", contadorLineas);
+        System.out.printf("Tiempo procesamiento (milisegundos): %d %n", time);
+    }
+}
